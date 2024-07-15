9937fb51bf4fe053af876ec291ffa5cc6589d912
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2024-07-15 11:00:04 -0300

    Desenvolvido componente botao do formulario

--- carro/src/Components/Formulario/index.js
+++ carro/src/Components/Formulario/index.js
@@ -1,3 +1,4 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
@@ -12,14 +13,23 @@ const time = [
     'Hyundai',
     'Ford'
 ]
+
+const aoSalvar = (evento) =>{
+    evento.preventDefault()
+    console.log('Form foi submetido')
+}
     return (
         <section className='formulario'>
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha o Card para Realizar o Cadastro de Carro</h2>
-                <CampoTexto label="nome"  placeholder="Digite Nome do Carro"/>
-                <CampoTexto label="Cor"  placeholder="Digite Cor do Carro"/>
-                <CampoTexto label="Ano"  placeholder="Digite Ano do Carro"/>
-                <ListaSuspensa itens={time} />
+                <CampoTexto obrigatorio={true} label="nome"  placeholder="Digite Nome do Carro"/>
+                <CampoTexto obrigatorio={true}  label="Cor"  placeholder="Digite Cor do Carro"/>
+                <CampoTexto obrigatorio={true}  label="Ano"  placeholder="Digite Ano do Carro"/>
+                <CampoTexto label="Imagem"  placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa obrigatorio={true}  itens={time} />
+                <Botao>
+                     Cadastrar Carro
+                </Botao>
             </form>
         </section>
     );
